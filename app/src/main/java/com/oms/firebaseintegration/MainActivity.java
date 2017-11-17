796ec42c9c8c2ed70bf6f4091f1090a06f75d499
Mahadev Dalavi <1990mimahadeve@gmail.com>
2017-11-17 11:40:39 +0530

    Initial commit

---
+++ app/src/main/java/com/oms/firebaseintegration/MainActivity.java
@@ -0,0 +1,60 @@
+package com.oms.firebaseintegration;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = database.getReference("message");
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                String data = dataSnapshot.getValue(String.class);
+                Log.d("test", "Value is: " + data);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+
+                Log.w("test", "Failed to read value.", error.toException());
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menuSettings) {
+            Toast.makeText(MainActivity.this, "Hi Mahadev", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
